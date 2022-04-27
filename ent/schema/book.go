d4624c90ce7e65b493ea873a0d47efe4d199e835
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-04-27 15:02:07 +0900

    add base fields for user and book

---
+++ ent/schema/book.go
@@ -0,0 +1,23 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+// Book holds the schema definition for the Book entity.
+type Book struct {
+	ent.Schema
+}
+
+// Fields of the Book.
+func (Book) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("name"),
+	}
+}
+
+// Edges of the Book.
+func (Book) Edges() []ent.Edge {
+	return nil
+}
---
+++ ent/schema/user.go
@@ -0,0 +1,32 @@
+package schema
+
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+// User holds the schema definition for the User entity.
+type User struct {
+	ent.Schema
+}
+
+// Fields of the User.
+func (User) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("username").
+			Unique(),
+		field.String("display_name"),
+		field.Time("created_at").
+			Default(time.Now),
+		field.Time("updated_at").
+			Default(time.Now).
+			UpdateDefault(time.Now),
+	}
+}
+
+// Edges of the User.
+func (User) Edges() []ent.Edge {
+	return nil
+}
