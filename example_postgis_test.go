9f72acae42cf39901be6bd7da82b4c81348a0506
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-05-16 15:07:47 +0900

    create restaurant

--- example_postgis_test.go
+++ example_postgis_test.go
@@ -3,9 +3,11 @@ package main_test
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"log"
 
 	"github.com/antonve/go-ent-experiment/ent"
+	"github.com/antonve/go-ent-experiment/ent/schema"
 
 	"entgo.io/ent/dialect"
 	entsql "entgo.io/ent/dialect/sql"
@@ -32,5 +34,21 @@ func ExamplePostGIS() {
 	if _, err := client.Restaurant.Delete().Exec(ctx); err != nil {
 		log.Fatalf("failed restaurants users: %v", err)
 	}
+
+	// Create restaurants
+	coco := createRestaurant(client, "CoCo Ichibanya Ebisu", 35.64699825984844, 139.71194575396922)
+
+	fmt.Println(coco.Name)
+
 	// Output:
+	// CoCo Ichibanya Ebisu
+}
+
+func createRestaurant(client *ent.Client, name string, long, lat float64) *ent.Restaurant {
+	r := client.Restaurant.Create().
+		SetName(name).
+		SetLocation(&schema.Point{long, lat}).
+		SaveX(context.Background())
+
+	return r
 }
